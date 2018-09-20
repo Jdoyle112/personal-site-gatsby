2af3c207138f0c0f7250cec6a1a9a41badffb28a
Jack <jdoyle112@gmail.comS>
2018-09-20 16:51:56 -0400

    Init commit

---
+++ src/components/home/hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import github from '../img/github-icon.svg'
+import logo from '../img/logo.svg'
+
+const Hero = () => (
+    <div className="hero-container">
+        <h1>Tasty nuggets for react developers</h1>
+        <span>Hi, I'm Jack. I like to share what I know about web development, and other random things.</span>
+    </div>
+)
+
+export default Hero
