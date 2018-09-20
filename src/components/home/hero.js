9ac8a931e998a3b97c6f297c213c9bbe68052c48
Jack <jdoyle112@gmail.comS>
2018-09-20 16:55:42 -0400

    Bug

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
 
 const Hero = () => (
     <div className="hero-container">
